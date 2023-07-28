5eb259d15d805483ebb0a6e9c4110f272e07097b
Maicon Filippsen <filippsen@universe.ai>
2023-07-28 14:23:58 -0300

    Add webpack configuration along with Tailwind and PostCSS preferences

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    content: [
+        //"./src/*.{html,js}",
+        "./dist/*.{html,js}",
+        "./index.html"
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                "quattrocento": ["Quattrocento", "sans-serif"] 
+            },
+
+            colors: {
+                bakgrund: {
+                    1: "#353B43",
+                    2: "#242930",
+                    3: "#1D2028"
+                },
+                bakgrundselement: "#33348E",
+
+                rubriker: "#FFFFFF",
+                underrubriker: "#AFBAC4",
+                brodtext: "#737F8A",
+
+                lankar: "#59BBF0",
+                lankarhover: "#1784BA",
+            }
+
+        },
+
+    },
+
+    plugins: [
+    ],
+}
---
+++ webpack.popup-config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    node: {
+        global: false
+    },
+    mode: "production",
+    entry: "./src/extension/popup/popup.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "popup.js",
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "popup.css"
+        }),
+    ], 
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    //{ loader: 'resolve-url-loader' },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                ],
+            },
+            {
+                test: /\.riot$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: "@riotjs/webpack-loader",
+                    options: {
+                        hot: false, // set it to true if you are using hmr
+                        // add here all the other @riotjs/compiler options riot.js.org/compiler
+                        // template: 'pug' for example
+                        scopedCss: true
+                    }
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+        }
+    }
+};
